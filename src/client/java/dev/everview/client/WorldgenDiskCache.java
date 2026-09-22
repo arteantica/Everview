0c01ceecb11131976a0b641965ed672044c31fb9
arteantica <arteantica123@gmail.com>
2026-09-22 16:15:15 -0700

    build: bump cache for split 4/8-block near rings

--- src/client/java/dev/everview/client/WorldgenDiskCache.java
+++ src/client/java/dev/everview/client/WorldgenDiskCache.java
@@ -28,7 +28,7 @@ import java.util.zip.GZIPOutputStream;
  */
 public final class WorldgenDiskCache {
     private static final int MAGIC = 0x45564C31; // EVL1
-    private static final int VERSION = 7;
+    private static final int VERSION = 8;
     private static final int MAX_TILES = 100_000;
     private static final int MAX_VERTEX_INTS = 1_000_000;
 
@@ -43,7 +43,7 @@ public final class WorldgenDiskCache {
 
         return server.getWorldPath(LevelResource.ROOT)
                 .resolve("everview")
-                .resolve("lod-cache-v7")
+                .resolve("lod-cache-v8")
                 .resolve(dimensionId + ".evc.gz");
     }
 
